ee19cc180d9038932f1a1cf1a5452cc880e6a1cb
campisano <riccardo.campisano@gmail.com>
2019-11-11 08:26:26 -0300

    refactor: board

--- src/test/board.cpp
+++ src/test/board.cpp
@@ -7,22 +7,10 @@ namespace
 const std::size_t EXPECTED_BOARD_SIZE = 16;
 }
 
-TEST_GROUP(BoardTest)
-{
-    Board * m_board;
-
-    void setup()
-    {
-        m_board = new Board();
-    }
-    void teardown()
-    {
-        delete m_board;
-    }
-};
+TEST_GROUP(BoardTest) {};
 
 TEST(BoardTest, Creation)
 {
-    CHECK(NULL != m_board);
-    CHECK_EQUAL(EXPECTED_BOARD_SIZE, m_board->size());
+    Board board;
+    CHECK_EQUAL(EXPECTED_BOARD_SIZE, board.size());
 }
