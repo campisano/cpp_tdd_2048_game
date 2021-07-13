b0b23dddebb7c69e32fbdc982daa4eb15af96660
campisano <riccardo.campisano@gmail.com>
2021-07-13 00:39:00 -0300

    feature:    CI - improve cache folder organization for coverage, update sonar scanner version

--- ci/custom/internal_coverage.sh
+++ ci/custom/internal_coverage.sh
@@ -14,44 +14,47 @@ apt-get -qq -y install --no-install-recommends wget unzip > /dev/null
 
 mv -n /var/cache/apt/* .custom_cache/var/cache/apt/
 
-mkdir -p .custom_cache/srv/sonar
-wget -P .custom_cache/srv/cache -c -nv --no-check-certificate https://sonarcloud.io/static/cpp/build-wrapper-linux-x86.zip
-unzip -d .custom_cache/srv/sonar -q -n .custom_cache/srv/cache/build-wrapper-linux-x86.zip
-wget -P .custom_cache/srv/cache -c -nv --no-check-certificate https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.4.0.2170-linux.zip
-unzip -d .custom_cache/srv/sonar -q -n .custom_cache/srv/cache/sonar-scanner-cli-4.4.0.2170-linux.zip
+wget -P .custom_cache/sonar/pkg -c -nv --no-check-certificate https://sonarcloud.io/static/cpp/build-wrapper-linux-x86.zip
+mkdir -p .custom_cache/sonar/wrapper
+unzip -d .custom_cache/sonar/wrapper -q -n .custom_cache/sonar/pkg/build-wrapper-linux-x86.zip
 
 make -e debug
 make -e clean
 
-.custom_cache/srv/sonar/build-wrapper-linux-x86/build-wrapper-linux-x86-64 --out-dir bw-output make -e debug
-
-
+.custom_cache/sonar/wrapper/build-wrapper-linux-x86/build-wrapper-linux-x86-64 --out-dir .bw-output make -e debug
 
 ./run.sh debug test
 
-mkdir -p gcov
-cd gcov
+mkdir -p .gcov
+cd .gcov
 for F in `find ../build/debug/CMakeFiles -name '*.cpp.o'`;
 do
   gcov -p "${F}" > /dev/null;
 done
 cd ..
 
+SONARSCAN_VER="4.6.2.2472"
+SONARSCAN_URL="https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-${SONARSCAN_VER}-linux.zip"
+mkdir -p .custom_cache/sonar/pkg
+wget -P .custom_cache/sonar/pkg -c -nv --no-check-certificate "${SONARSCAN_URL}"
+mkdir -p .custom_cache/sonar/scanner
+unzip -d .custom_cache/sonar/scanner -q -n ".custom_cache/sonar/pkg/sonar-scanner-cli-${SONARSCAN_VER}-linux.zip"
 
-export SONAR_TOKEN
-export SONAR_USER_HOME=.custom_cache/srv/cache/sonar
-.custom_cache/srv/sonar/sonar-scanner-4.4.0.2170-linux/bin/sonar-scanner \
+mkdir -p .custom_cache/sonar/home
+export SONAR_USER_HOME=.custom_cache/sonar/home
+.custom_cache/sonar/scanner/sonar-scanner-${SONARSCAN_VER}-linux/bin/sonar-scanner \
     -Dsonar.host.url=https://sonarcloud.io \
     -Dsonar.organization=${SONAR_ORGANIZATION} \
     -Dsonar.projectKey=${SONAR_PROJECT} \
     -Dsonar.scm.provider=git \
+    -Dsonar.working.directory=.scannerwork \
     -Dsonar.sources=src \
     -Dsonar.sourceEncoding=UTF-8 \
-    -Dsonar.working.directory=.scannerwork \
+    -Dsonar.coverage.exclusions=src/test/** \
     -Dsonar.cfamily.threads=1 \
-    -Dsonar.cfamily.gcov.reportsPath=gcov \
-    -Dsonar.cfamily.build-wrapper-output=bw-output \
+    -Dsonar.cfamily.gcov.reportsPath=.gcov \
+    -Dsonar.cfamily.build-wrapper-output=.bw-output \
     -Dsonar.cfamily.cache.enabled=false
 
-rm -rf bw-output gcov .scannerwork
+rm -rf .bw-output .gcov .scannerwork
 make distclean
